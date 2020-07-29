34830bf92c92ce1d250bbceec6f803bf7d3026d1
avishith <pmshithu143@gmail.com>
2020-07-30 01:41:27 +0530

    chat bot under modification

--- Chatbot.py
+++ Chatbot.py
@@ -5,11 +5,18 @@ ABC=Frame(root,bg='#010101',bd=20,relief=RIDGE)
 ABC.grid()
 
 def sent():
-	txt.config(state=NORMAL)
+#	root=Tk()
+	txt=Text(ABC,height=2,width=40,padx=10,pady=10)
+	txt.tag_config("right", justify="right")
+	txt.pack()
+	
+	#txt.config(state=NORMAL)
+	
 	
 	v=e.get()
 	sent="You => " + v
 	txt.insert(END,"\n"+sent)
+
 	
 	if v=='hai':
 		a='hello'+" <== Bot "
@@ -38,12 +45,11 @@ def sent():
 ABC1=Frame(root,bd=20,)
 ABC1.grid()
 
-txt=Text(ABC,height=30,width=40,padx=10,pady=10)
-txt.tag_config("right", justify="right")
+
 
 #txt.tag_configure("green", foreground="green")
 
-txt.grid(column=0,row=0)
+#txt.grid(column=0,row=0)
 
 e=Entry(ABC1,width=30,bd=2,insertwidth=5,font=("aril",7))
 e.grid(row=1,column=0)
